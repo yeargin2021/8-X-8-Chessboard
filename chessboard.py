7108978467748f059245f72bc84a009d3a40df7e
Tommy H. Yeargin, Jr <88904457+yeargin2021@users.noreply.github.com>
2024-12-27 16:35:38 -0500

    commit

--- chessboard.py
+++ chessboard.py
@@ -1,5 +1,7 @@
 x = "__|"
 print("   " + ("__ " * 8))
+a = 8
 for i in range(8):
-    print(str(i + 1) + " |"+ (x * 8))
+    print(str(a) + " |"+ (x * 8))
+    a = a - 1
 print("   a  b  c  d  e  f  g  h")
